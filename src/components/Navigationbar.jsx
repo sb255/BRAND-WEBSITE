c7cd7c88246c442e6888de49d977f58731d50a3f
admin <borrisden@Samarths-iMac.local>
2026-08-15 14:54:20 +0530

    refs added

--- src/components/Navigationbar.jsx
+++ src/components/Navigationbar.jsx
@@ -15,16 +15,22 @@ function Navigationbar(){
                 </div>
                 <div className='flex justify-end h-24 px-2 py-2'>
                     <div className="flex flex-nowrap justify-center items-center overflow-hidden h-18 w-18 rounded-full border-5 border-orange-200 hover:translate-1">
-                        <img className="h-12 w-12 px-0 py-0 mx-0 my-0" src={instagram}></img>
+                        <a href="https://www.instagram.com/groxifi" target="_blank" rel="noopener noreferrer"> 
+                            <img className="h-12 w-12 px-0 py-0 mx-0 my-0" src={instagram}></img>
+                        </a>
                     </div>
                     <div className="flex flex-nowrap justify-center items-center overflow-hidden h-18 w-18 rounded-full border-5 border-orange-200 hover:translate-1">
-                        <img className="h-12 w-12 px-0 py-0 mx-0 my-0" src={facebook}></img>
+                        <a href="https://www.facebook.com/groxifi.official" target="_blank" rel="noopener noreferrer"> 
+                            <img className="h-12 w-12 px-0 py-0 mx-0 my-0" src={facebook}></img>
+                        </a>
                     </div>
                     <div className="flex flex-nowrap justify-center items-center overflow-hidden h-18 w-18 rounded-full border-5 border-orange-200 hover:translate-1">
                         <img className="h-12 w-12 px-0 py-0 mx-0 my-0" src={whatsapp}></img>
                     </div>
                     <div className="flex flex-nowrap justify-center items-center overflow-hidden h-18 w-18 rounded-full border-5 border-orange-200 hover:translate-1">
-                        <img className="h-12 w-12 px-0 py-0 mx-0 my-0" src={youtube}></img>
+                        <a href="https://www.youtube.com/@Groxifi" target="_blank" rel="noopener noreferrer"> 
+                            <img className="h-12 w-12 px-0 py-0 mx-0 my-0" src={youtube}></img>
+                        </a>
                     </div>
                 </div>
             </div>
