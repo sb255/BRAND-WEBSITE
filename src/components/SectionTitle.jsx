49a5616271fa2c9dc3faa3d92ef5157c30a0979b
admin <borrisden@Samarths-iMac.local>
2026-08-19 03:34:25 +0530

    Tailwind

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -2,7 +2,54 @@ function SectionTitle(props){
 
     return(
         <>
-            <div className="flex justify-center mx-2 my-2 px-2.5 py-2.5 gap-1 bg-amber-100 text-2xl font-bold font-mono rounded-3xl hover:translate-2">{props.title}</div>
+            <div class="h-30 relative flex justify-center items-center overflow-hidden my-5">
+                <div class="grid grid-cols-20 h-100 w-full opacity-50">
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+            </div>
+            <div class="absolute grid grid-rows-20 h-100 w-full z-1 opacity-50">
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+                <div class="border-2 opacity-30"></div>
+            </div>
+            <div class="absolute font-serif xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-600">{props.title}</div>     
+            </div>
         </>
     )
 
