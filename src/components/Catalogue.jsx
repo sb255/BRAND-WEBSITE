0ff4c85e37b5b015a4f35b615ecee56bb52f28fc
admin <borrisden@Samarths-iMac.local>
2026-08-21 00:37:04 +0530

    Catalogue updated

--- src/components/Catalogue.jsx
+++ src/components/Catalogue.jsx
@@ -6,8 +6,8 @@ function Catalogue(props){
                 <div class="h-7/10 w-10/10 md:h-8/10 px-5 py-5">
                     <img class="h-1/1 w-1/1" src={props.productImage}></img>
                 </div>
-                <div class="h-3/10 w-10/10 md:h-2/10 px-5 py-5 font-bold">
-                    <h5 class="card-title my-2">{props.cardTitle}</h5> 
+                <div class="flex justify-center items-center h-3/10 w-10/10 md:h-2/10 text-xl font-extrabold font-stretch-expanded">
+                    <div class="animate-bounce">{props.cardTitle}</div>   
                 </div>
             </div>
             
