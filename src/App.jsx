1ff4fe75aaa75af7654c046cc51b837bcd271583
admin <samarthbais1@gmail.com>
2026-09-11 01:50:53 +0530

    UNDO A CHANGE

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,6 @@ function App() {
   return (
     <>
       <Navigationbar />
-      <Test />
       <Landingpage />
       <Mission />
       <SectionTitle title={'VIEW OUR TOP QUALITY PRODUCTS'} iconNumber={0}/>
